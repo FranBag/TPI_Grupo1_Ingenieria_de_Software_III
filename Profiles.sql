b7e3e464d65697cbcf60468e8abe8b2f3f7021c4
GUTomasMateo <GU.MateoTomas.894@gmail.com>
2026-04-24 19:26:15 -0300

    Modificacion pra error

--- Profiles.sql
+++ Profiles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Profiles (
     profile_id SERIAL PRIMARY KEY,
-    bio TEXT,
+    bio TEXTO,
     birth_date DATE NOT NULL, -- Usado para calcular rango de edad
     gender VARCHAR(50),
     location_address VARCHAR(255),
