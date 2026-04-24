f773a9a7c1c19cf5c42816ae158475ab76a683fb
GUTomasMateo <GU.MateoTomas.894@gmail.com>
2026-04-23 21:36:11 -0300

    Actualizado Profiles

---
+++ Profiles.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Profiles (
+    profile_id SERIAL PRIMARY KEY,
+    bio TEXT,
+    birth_date DATE NOT NULL, -- Usado para calcular rango de edad
+    gender VARCHAR(50),
+    location_address VARCHAR(255),
+    city VARCHAR(100),
+    sports_interests TEXT,
+    search_radius INTEGER, -- En kilómetros
+    languages TEXT
+);
